791218421f3fcf805136f3493481d8b16b53fbe5
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2025-06-29 15:09:48 +0900

    a

---
+++ DNC_HelloWorld1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DNC_HelloWorld1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
